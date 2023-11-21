eade70aac6117c726ee061ba1b99a213bf486ce2
Aditya Rajiv <adimantheboss123@gmail.com>
2023-11-21 18:05:33 +0530

    updated app.py by converting recommendations to list and then st.text

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ selected_game_name = st.selectbox(
 if st.button('recommend'):
     try:
         recommendations = recommendation_system(selected_game_name, game)
-        for game_name in recommendations.tolist():
+        for game_name in list(recommendations):
             st.text(game_name)
         # st.dataframe(pd.DataFrame(recommendations, columns = ['Game_names']))
     except Exception as e:
